4344b1d20edf8a7039c0399301f8d54dc0db184d
Antonio Portilla López <antport@gmail.com>
2019-07-03 15:37:52 +0200

    añadimos estructura y datos. asi como el diagrama dbforce

--- base de datos.sql
+++ base de datos.sql
@@ -1,6 +1,6 @@
 ﻿USE db2019;
 CREATE TABLE personas(
-id_persona int AUTO_INCREMENT PRIMARY KEY,
+  id_persona int AUTO_INCREMENT PRIMARY KEY,
   persona varchar(31),
   UNIQUE (persona)
   );
@@ -9,6 +9,15 @@ CREATE TABLE lugares(
   id_lugar int AUTO_INCREMENT PRIMARY KEY,
   lugar varchar (31),
   UNIQUE(lugar)
-  
-  
-  );
+ );
+
+CREATE TABLE viajaran(
+  id_persona int,
+  id_lugar int,
+  fecha date,
+  PRIMARY KEY (id_persona,id_lugar),
+  FOREIGN KEY (id_persona) REFERENCES personas(id_persona),
+  FOREIGN KEY (id_lugar) REFERENCES lugares(id_lugar)
+);
+
+ALTER TABLE viajaran ADD UNIQUE(id_persona,id_lugar);
---
+++ datos.sql
@@ -0,0 +1,15 @@
+﻿USE db2019;
+
+INSERT INTO personas (persona) VALUES
+  ('Isabel'),('Antonio'),('Jhonny'),('Roberto'),('María');
+
+INSERT INTO lugares (lugar) VALUES
+  ('China'),('Hawai'),('Bogotá'),('Madrid'),('New York');
+
+INSERT INTO viajaran (id_persona,id_lugar) VALUES
+  (1,1),(2,2),(3,3),(4,4),(5,5);
+
+INSERT INTO lugares (lugar) VALUES ('Noja');
+
+INSERT INTO viajaran (id_lugar,id_persona)
+  VALUES (2,4),(4,5),(5,3);
